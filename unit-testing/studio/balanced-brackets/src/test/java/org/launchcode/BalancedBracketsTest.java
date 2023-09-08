a03fef4b110fef9f52a0a0cbc2eb257c8db5a419
Ashley Hainline <aetindle@gmail.com>
2023-09-08 13:22:57 -0500

    code for unit testing studio

--- unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
+++ unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
@@ -7,7 +7,48 @@ import static org.junit.jupiter.api.Assertions.*;
 class BalancedBracketsTest {
     //TODO: add tests here
     @Test
-    public void emptyTest() {
-        assertEquals(true, true);
+    public void emptyStringReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
     }
+
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void multipleSetsOfBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[][][][][]"));
+    }
+
+    @Test
+    public void multipleBracketsWithOtherCharsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[{dkdjaflk}], kasldfj [ adkslfj ]"));
+    }
+
+    @Test
+    public void correctlyNestedBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[[[]]]]"));
+    }
+
+    @Test
+    public void mismatchedBracketSetsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void allRightBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[[[["));
+    }
+
+    @Test
+    public void allLeftBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]]]]]"));
+    }
+
+    @Test
+    public void mismatchedNestedBracketsReturnFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[[[[[[[]]]]]]]"));
+    }
+
 }
